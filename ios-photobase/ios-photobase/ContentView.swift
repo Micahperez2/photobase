f513ffa3e2a7fe7f087afbd0323b4e26631cb0cd
Micah Perez <micah@Micahs-MacBook-Air.local>
2022-02-27 16:44:18 -0800

    feat: allow users to use short passcode over url

--- ios-photobase/ios-photobase/ContentView.swift
+++ ios-photobase/ios-photobase/ContentView.swift
@@ -8,36 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name: String = "Tim"
+    @State private var name: String = ""
+    @State private var isvalidURL: Bool = false
 
     var body: some View {
         VStack(alignment: .center) {
-            TextField("Enter your name", text: $name)
+            TextField("Enter your domain", text: $name)
                 .textFieldStyle(.roundedBorder)
                 .padding()
                 .onSubmit {
-                    print(validateFunction(domain: name))
+                    print(validateFunction(domain: ("https://" + name + ".loca.lt")))
                 }
-            Text(validateFunction(domain: name))
+            Text(validateFunction(domain: ("https://" + name + ".loca.lt")))
                 .padding()
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                .foregroundColor(textColor(input: ("https://" + name + ".loca.lt")))
+            Button("Open Camera") {
             }
             .padding()
         }
     }
     func validateFunction(domain: String) -> String {
-
-        if (domain == "Hello") {
-            return "Good"
-        }
-        else if (domain == "World") {
-            return "Caution"
+        validURL(input: domain)
+        if (isvalidURL) {
+            return "Domain Online"
         }
         else {
-            return "Bad"
+            return "Domain Offline"
         }
-        
+    }
+    func textColor(input: String) -> Color {
+
+        if (isvalidURL) {
+            return Color.green
+        }
+        else {
+            return Color.red
+        }
+    }
+    
+    func validURL(input: String) -> Void {
+        if input != "" {
+            let url1 = URL(string: input)!
+            url1.isReachable { success in
+                if success {
+                    isvalidURL = true
+                    print("url1 is reachable")  // url1 is reachable
+                } else {
+                    isvalidURL = false
+                    print("url1 is unreachable") // url1 is unreachable
+                }
+            }
+        }
     }
 }
 
@@ -46,5 +67,15 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension URL {
+    func isReachable(completion: @escaping (Bool) -> ()) {
+        var request = URLRequest(url: self)
+        request.httpMethod = "HEAD"
+        URLSession.shared.dataTask(with: request) { _, response, _ in
+            completion((response as? HTTPURLResponse)?.statusCode == 200)
+        }.resume()
     }
 }
