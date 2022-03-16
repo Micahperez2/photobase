ae8d462f1a6e4d275d047787a039ec5428fab1cb
Micah Perez <micahperez99@gmail.com>
2022-03-15 23:19:29 -0700

    fix: restructured files

--- ios-photobase/ios-photobase/ContentView.swift
+++ ios-photobase/ios-photobase/ContentView.swift
@@ -10,21 +10,34 @@ import SwiftUI
 struct ContentView: View {
     @State private var name: String = ""
     @State private var isvalidURL: Bool = false
-
+    let offBlack = Color(red: 0.11, green: 0.11, blue: 0.12)
+    let photoPurple = Color(red: 0.729, green: 0.333, blue: 0.827)
+    
     var body: some View {
-        VStack(alignment: .center) {
-            TextField("Enter your domain", text: $name)
-                .textFieldStyle(.roundedBorder)
-                .padding()
-                .onSubmit {
-                    print(validateFunction(domain: ("https://" + name + ".loca.lt")))
+            ZStack{
+                offBlack
+                .ignoresSafeArea(.all)
+                
+                VStack(alignment: .center) {
+                Text("Photobase")
+                    .font(.custom("Chalkduster", size: 60))
+                    .foregroundColor(photoPurple)
+                    .frame(width: 350, height: 250, alignment: .top)
+                TextField("Enter your Passcode", text: $name)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.vertical, 25)
+                    .padding(.horizontal, 40)
+                    .foregroundColor(photoPurple)
+                    .onSubmit {
+                        print(validateFunction(domain: ("https://" + name + ".loca.lt")))
+                    }
+                Text(validateFunction(domain: ("https://" + name + ".loca.lt")))
+                    .padding()
+                    .foregroundColor(textColor(input: ("https://" + name + ".loca.lt")))
+                Button("Open Camera") {
                 }
-            Text(validateFunction(domain: ("https://" + name + ".loca.lt")))
                 .padding()
-                .foregroundColor(textColor(input: ("https://" + name + ".loca.lt")))
-            Button("Open Camera") {
             }
-            .padding()
         }
     }
     func validateFunction(domain: String) -> String {
@@ -46,9 +59,9 @@ struct ContentView: View {
         }
     }
     
-    func validURL(input: String) -> Void {
-        if input != "" {
-            let url1 = URL(string: input)!
+    func validURL(input: String?) -> Void {
+        if let input = input {
+            guard let url1 = URL(string: input) else { return }
             url1.isReachable { success in
                 if success {
                     isvalidURL = true
@@ -66,7 +79,10 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
 
