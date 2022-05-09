efcaeb64a8babcad1ee9b142c4835c5514e45054
Micahperez2 <Micahperez2@users.noreply.github.com>
2022-05-09 16:14:35 +0000

    Prettified Code!

--- Photobase-Desktop/server.js
+++ Photobase-Desktop/server.js
@@ -12,23 +12,22 @@ app.use(express.urlencoded({ extended: true }));
 //Used to look for static files (css) in public folder
 //app.use(express.static("public"));
 // Require static assets from public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // set the view engine to ejs
 //app.set("view engine", "ejs");
 //app.set("views", __dirname);
 
-/* Test */ 
+/* Test */
 
 // Set view engine as EJS
-app.engine('ejs', require('ejs').renderFile);
-app.set('view engine', 'ejs');
-// Set 'views' directory for any views 
+app.engine("ejs", require("ejs").renderFile);
+app.set("view engine", "ejs");
+// Set 'views' directory for any views
 // being rendered res.render()
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 
 /* End of test */
-
 
 var photos = [];
 var online_url = "";
